f8a1d35cbe77280c9ceb8f78fcf39ed629a5a07d
Daniel Dawson <dannyldawson@gmail.com>
2018-09-12 14:40:58 -0700

    Change GridLayout to GridView, add adapter
    Gridview is more appropriate for this program, as we do not know how many movies
    will be accessed via our api request. Leaving adapter logic until networking
    code is written.

--- app/src/main/java/com/maurdan/flaco/udacitynd_project2_popularmovies/MainActivity.java
+++ app/src/main/java/com/maurdan/flaco/udacitynd_project2_popularmovies/MainActivity.java
@@ -2,6 +2,8 @@ package com.maurdan.flaco.udacitynd_project2_popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.BaseAdapter;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridLayout gridLayout = findViewById(R.id.grid_layout);
+
     }
 }
---
+++ app/src/main/java/com/maurdan/flaco/udacitynd_project2_popularmovies/adapters/GridViewAdapter.java
@@ -0,0 +1,35 @@
+package com.maurdan.flaco.udacitynd_project2_popularmovies.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class GridViewAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    public GridViewAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
