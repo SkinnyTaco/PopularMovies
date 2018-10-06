05e94a9ec7ac0580d0c7f38a7b6bb1f1c0e38a94
Daniel Dawson <dannyldawson@gmail.com>
2018-10-05 18:52:51 -0700

    Add RecyclerView dependencies, adapter

---
+++ app/src/main/java/com/maurdan/flaco/udacitynd_project2_popularmovies/adapters/GridLayoutAdapter.java
@@ -0,0 +1,31 @@
+package com.maurdan.flaco.udacitynd_project2_popularmovies.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GridLayoutAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
